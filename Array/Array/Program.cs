6fb68fb9ae4dfe4cb45e95b89322d51c6b8b2e1c
Suryash-00 <suryash35@gmail.com>
2022-09-13 17:38:53 +0530

    C sharp basics

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -24,6 +24,7 @@ public class ArrayExample
     }
 }
 */
+/*
 using System;  
 public class MultiArrayExample
 {
@@ -42,3 +43,50 @@ public class MultiArrayExample
         }
     }
 }
+*/
+/*
+using System;
+
+namespace CSharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an array  
+            int[] arr = new int[6] { 5, 8, 9, 25, 0, 7 };
+            // Creating an empty array  
+            int[] arr2 = new int[6];
+
+            // Displaying length of array  
+            Console.WriteLine("length of first array: " + arr.Length);
+            // Sorting array  
+            Array.Sort(arr);
+            Console.Write("First array elements: ");
+            // Displaying sorted array  
+            PrintArray(arr);
+            
+            // Finding index of an array element  
+            Console.WriteLine("\nIndex position of 25 is " + Array.IndexOf(arr, 25));
+
+            // Coping first array to empty array  
+            Array.Copy(arr, arr2, arr.Length);
+            Console.Write("Second array elements: ");
+
+            // Displaying second array  
+            PrintArray(arr2);
+            Array.Reverse(arr);
+            Console.Write("\nFirst Array elements in reverse order: ");
+            PrintArray(arr);
+        }
+        // User defined method for iterating array elements  
+        static void PrintArray(int[] arr)
+        {
+            foreach (Object elem in arr)
+            {
+                Console.Write(elem + " ");
+            }
+        }
+    }
+}
+*/
---
+++ OOPs/OOPs/Program.cs
@@ -0,0 +1,125 @@
+﻿/*
+using System;
+public class Employee
+{
+    public int id;
+    public String name;
+    public float salary;
+    public void insert(int i, String n, float s)
+    {
+        id = i;
+        name = n;
+        salary = s;
+    }
+    public void display()
+    {
+        Console.WriteLine(id + " " + name + " " + salary);
+    }
+}
+class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+        e1.insert(101, "Suryash", 890000f);
+        e2.insert(102, "Nikhilesh", 490000f);
+        e1.display();
+        e2.display();
+
+    }
+}
+*/
+/*
+using System;
+
+namespace CsharpDestructor
+{
+
+    class Person
+    {
+
+        public Person()
+        {
+            Console.WriteLine("Constructor called.");
+        }
+
+        // destructor
+        ~Person()
+        {
+            Console.WriteLine("Destructor called.");
+        }
+
+        public static void Main(string[] args)
+        {
+
+            //creates object of Person
+            Person p1 = new Person();
+        }
+    }
+}
+*/
+
+/*
+// this keyword
+using System;
+public class Employee
+{
+    public int id;
+    public String name;
+    public float salary;
+    public Employee(int id, String name, float salary)
+    {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+    public void display()
+    {
+        Console.WriteLine(id + " " + name + " " + salary);
+    }
+}
+class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        Employee e1 = new Employee(101, "Sonoo", 890000f);
+        Employee e2 = new Employee(102, "Mahesh", 490000f);
+        e1.display();
+        e2.display();
+
+    }
+}
+*/
+//static constructor
+using System;
+public class Account
+{
+    public int id;
+    public String name;
+    public static float rateOfInterest;
+    public Account(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+    static Account()
+    {
+        rateOfInterest = 9.5f;
+    }
+    public void display()
+    {
+        Console.WriteLine(id + " " + name + " " + rateOfInterest);
+    }
+}
+class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        Account a1 = new Account(101, "Sonoo");
+        Account a2 = new Account(102, "Mahesh");
+        a1.display();
+        a2.display();
+
+    }
+}
