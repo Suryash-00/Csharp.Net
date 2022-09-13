4c7ccf7726436f8c4d6e45c9a34d292d44623534
Suryash-00 <suryash35@gmail.com>
2022-09-13 15:13:37 +0530

    first commit

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+using System;
+
+// Call By Value
+namespace Functions
+{
+    class Program
+    {
+        // User defined function  
+        public void Show(int val)
+        {
+            val = val*val; // Manipulating value  
+            Console.WriteLine("Value inside the show function " + val);
+            // No return statement  
+        }
+        // Main function, execution entry point of the program  
+        static void Main(string[] args)
+        {
+            int val = 50;
+            Program program = new Program(); // Creating Object  
+            Console.WriteLine("Value before calling the function " + val);
+            program.Show(val); // Calling Function by passing value            
+            Console.WriteLine("Value after calling the function " + val);
+        }
+    }
+}
+*/
+using System;
+/*      // Call By Reference
+namespace Functions
+{
+    class Program
+    {
+        // User defined function  
+        public void Show(ref int val)
+        {
+            val = val*val; // Manipulating value  
+            Console.WriteLine("Value inside the show function " + val);
+            // No return statement  
+        }
+        // Main function, execution entry point of the program  
+        static void Main(string[] args)
+        {
+            int val = 50;
+            Program program = new Program(); // Creating Object  
+            Console.WriteLine("Value before calling the function " + val);
+            program.Show(ref val); // Calling Function by passing reference            
+            Console.WriteLine("Value after calling the function " + val);
+        }
+    }
+}
+*/
+namespace Functions
+//Out Parameter is useful when passing more than 1 value.
+{
+    class Program
+    {
+        // User defined function  
+        public void Show(out int a, out int b) // Out parameter  
+        {
+            int square = 5;
+            a = square;
+            b = square;
+            // Manipulating value  
+            a = a*a;
+            b = b*b;
+        }
+        // Main function, execution entry point of the program  
+        static void Main(string[] args)
+        {
+            int val1 = 50, val2 = 100;
+            Program program = new Program(); // Creating Object  
+            Console.WriteLine("Value before passing \n val1 = " + val1 + " \n val2 = " + val2);
+            program.Show(out val1, out val2); // Passing out argument  
+            Console.WriteLine("Value after passing \n val1 = " + val1 + " \n val2 = " + val2);
+        }
+    }
+}
---
+++ Miscellaneous/Miscellaneous/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Random
+{
+    class Rand
+    {
+        static void Main(string[] args)
+        {
+            /*
+               var names = new List<string> { "Suryash", "Deepak", "Anurag" };
+               names.Add("Arjun");
+               names.Add("Vivek");
+               names.Add("Yash");
+               names.Remove("Vivek");
+               foreach (string name in names)
+               {
+                   Console.WriteLine($"Hello {name.ToUpper()}");
+               }
+               Console.WriteLine(names[1]);
+               int index = names.IndexOf("Yash");
+               Console.WriteLine(index);
+               names.Sort();
+               for (int i=0; i<names.Count; i++)
+               {
+                   Console.WriteLine(names[i]);
+               }
+           }
+       }
+   }
+   */
+
+            //Fibonacci Numbers
+            
+            var fibo = new List<int> { 1, 1 };
+
+            while (fibo.Count < 10)
+            {
+                var previous = fibo[fibo.Count - 1];
+                var previous2 = fibo[fibo.Count - 2];
+                fibo.Add(previous + previous2);
+            }
+            foreach (var item in fibo)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
