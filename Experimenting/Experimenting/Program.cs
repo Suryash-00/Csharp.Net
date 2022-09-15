382b5302cc814001e82349b06a0cd6181b5daee7
Suryash-00 <suryash35@gmail.com>
2022-09-15 12:43:54 +0530

    Function Modification

---
+++ Experimenting/Experimenting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Class1
+{
+    public static void Even(int i)
+    {
+        for (int item = 0; item <= i; item++)
+        {
+            if (item % 2 == 0)
+            {
+                Console.WriteLine($"{item} is an even number");
+            }
+            else
+            {
+                Console.WriteLine($"{item} is not an even number");
+            }
+        }
+    }
+    public static void Main()
+    {
+        Class1.Even(15);
+    }
+}
