972867e13d466ce8d76a4d5d65ea4bc1182e5cdb
Suryash-00 <suryash35@gmail.com>
2022-10-03 11:43:01 +0530

    Controller Complete

---
+++ UnderstandingController/UnderstandingController/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using UnderstandingController.Models;
+
+namespace UnderstandingController.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWebHostEnvironment hostingEnvironment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            hostingEnvironment = environment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile photo)
+        {
+            // code to save the uploaded file in wwwroot folder
+            using (var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath, photo.FileName), FileMode.Create))
+            {
+                await photo.CopyToAsync(stream);
+            }
+
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ReceivedDataByRequest()
+        {
+            string name = Request.Form["name"];
+            string sex = Request.Form["sex"];
+            return View("ReceivedDataByRequest", $"{name} sex is {sex}");
+        }
+        public IActionResult ReceivedDataByParameter(string name, string sex)
+        {
+            return View("ReceivedDataByParameter", $"{name} sex is {sex}");
+        }
+        public IActionResult ReceivedDataByModelBinding(Person person)
+        {
+            return View("RecievedDataByModelBinding", person);
+        }
+    }
+}
---
+++ UnderstandingController/UnderstandingController/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace UnderstandingController.Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string sex { get; set; }
+    }
+}
