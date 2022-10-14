4ecec4b1779f3ac49af5f3d8f861bd0555465846
Suryash-00 <suryash35@gmail.com>
2022-10-14 18:00:19 +0530

    Razor_and_OOPs

---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewClass obj1= new NewClass();
+            obj1.str = "Suryash";
+            obj1.num = 30;
+            
+            NewClass obj2 = new NewClass();
+            obj2.str = "Rastogi";
+            obj2.num = 50;
+
+            NewClass obj3= new NewClass();
+            obj3 = obj1 + obj2;
+
+            Console.WriteLine(obj3.str);
+            Console.WriteLine(obj3.num);
+        }
+    }
+
+    class NewClass
+    {
+        public string str;
+        public int num;
+        public static NewClass operator +(NewClass obj1, NewClass obj2)
+        {
+            NewClass obj3= new NewClass();
+            obj3.str = obj1.str + obj2.str;
+            obj3.num = obj1.num + obj2.num;
+            return obj3;
+        }
+    }
+}
---
+++ ViewImportsDemo/ViewImportsDemo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ViewImportsDemo.Models;
+
+namespace ViewImportsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student std1 = new Student()
+            {
+                id = 1,
+                name = "Suryash",
+                age = 23
+            };
+            Student std2 = new Student()
+            {
+                id = 2,
+                name = "Yash",
+                age = 24
+            };
+            Student std3 = new Student()
+            {
+                id = 3,
+                name = "Daksh",
+                age = 22
+            };
+            Student std4 = new Student()
+            {
+                id = 3,
+                name = "Piyush",
+                age = 24
+            };
+
+            List<Student> std_list = new List<Student>();
+            std_list.Add(std1);
+            std_list.Add(std2);
+            std_list.Add(std3);
+            std_list.Add(std4);
+            
+            return View();
+        }
+    }
+}
