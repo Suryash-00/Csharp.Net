6807c3bf868e24f75e6750dd9f35f30d8ea73904
Suryash-00 <suryash35@gmail.com>
2022-10-17 12:28:39 +0530

    ViewData Demo

---
+++ ViewData_Demo/ViewData_Demo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using ViewData_Demo.Models;
+
+namespace ViewData_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["var1"] = "Message came from ViewData";
+            ViewData["var2"]= DateTime.Now.ToLongDateString();
+
+            string[] suparr = { "Iron Man", "Spider Man", "Batman", "Daredevil" };
+            ViewData["var3"]= suparr;
+
+            ViewData["var4"] = new List<string>()
+            {
+                "Cricket",
+                "Badminton",
+                "Basketball"
+            };
+
+            Student Suri= new Student();
+            Suri.Name = "Suryash Rastogi";
+            Suri.Sex = "Male";
+            Suri.Standard = 0;
+            ViewData["var5"]= Suri;
+
+            return View();
+        }
+    }
+}
---
+++ ViewData_Demo/ViewData_Demo/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewData_Demo.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public int Standard { get; set; }
+    }
+}
