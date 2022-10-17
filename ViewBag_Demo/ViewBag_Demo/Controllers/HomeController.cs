bba8e00d7f78b14979c69be8729dac1a44afdf33
Suryash-00 <suryash35@gmail.com>
2022-10-17 13:57:15 +0530

    ViewBag demo

--- ViewBag_Demo/ViewBag_Demo/Controllers/HomeController.cs
+++ ViewBag_Demo/ViewBag_Demo/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using ViewBag_Demo.Models;
 
 namespace ViewBag_Demo.Controllers
 {
@@ -6,6 +9,26 @@ namespace ViewBag_Demo.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.var1 = "Data came from ViewBag";
+
+            ViewBag.var2 = DateTime.Now.ToShortDateString();
+            
+            string[] arr = { "Suryash", "Yash", "Daksh" };
+            ViewBag.var3 = arr;
+            
+            ViewBag.var4= new List<string>()
+            {
+                "Prototype",
+                "Medal of Honour",
+                "Mafia"
+            };
+
+            Employee Suri = new Employee();
+            Suri.Name = "Suryash Rastogi";
+            Suri.Designation = "Intern";
+            Suri.City = "Lucknow";
+            ViewBag.var5= Suri;
+
             return View();
         }
     }
---
+++ ViewBag_Demo/ViewBag_Demo/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewBag_Demo.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string City { get; set; }
+    }
+}
