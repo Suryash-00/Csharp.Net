fa64bf82b52f959acd5ee7800b862aefba9badc0
Suryash-00 <suryash35@gmail.com>
2022-10-25 13:15:00 +0530

    RequiredAttribute

---
+++ Form_Required_Validation/Form_Required_Validation/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Form_Required_Validation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Form_Required_Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Employee e)
+        {
+            return View();
+        }
+        
+        /*public string Index(Employee e)
+        {
+            if (ModelState.IsValid)
+            {
+                return "Name is: " + e.Name + " Gender is: " + e.Gender + " Age is: " + e.Age;
+            }
+            else
+            {
+                return "Validation Failed";
+            }
+        }*/
+    }
+}
---
+++ Form_Required_Validation/Form_Required_Validation/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Form_Required_Validation.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Fill the name you dumb!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Are you gender-less?")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "So you aren't borned.....")]
+        public int? Age { get; set; }
+    }
+}
