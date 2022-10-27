d3a144084587eb0c8e5d6ba8f454e871589b38af
Suryash-00 <suryash35@gmail.com>
2022-10-27 16:13:10 +0530

    learning injection

---
+++ Constructor_Injection/Constructor_Injection/Program.cs
@@ -0,0 +1,100 @@
+﻿// Example of Tightly Coupled
+/*
+using System;
+
+namespace Constructor_Injection
+{
+    class CurrentAccount
+    {
+        public void printDetails()
+        {
+            Console.WriteLine("Details of current account");
+        }
+    }
+
+    class SavingsAccount
+    {
+        public void printDetails()
+        {
+            Console.WriteLine("Details of savings account");
+        }
+    }
+    
+    class Account
+    {
+        CurrentAccount ca= new CurrentAccount();
+
+        SavingsAccount sa= new SavingsAccount();
+
+        public void printAccounts()
+        {
+            ca.printDetails();
+            sa.printDetails();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account();
+            a.printAccounts();
+        }
+    }
+}
+*/
+
+// Dependency Injection through Constructor Injection
+
+using System;
+
+namespace Constructor_Injection
+{
+    public interface IAccount
+    {
+        void printDetails();
+    }
+    
+    class CurrentAccount: IAccount
+    {
+        public void printDetails()
+        {
+            Console.WriteLine("Details of current account");
+        }
+    }
+
+    class SavingsAccount: IAccount
+    {
+        public void printDetails()
+        {
+            Console.WriteLine("Details of savings account");
+        }
+    }
+
+    class Account
+    {
+        private IAccount account;
+
+        public Account(IAccount account)
+        {
+            this.account = account;
+        }
+
+        public void printAccounts()
+        {
+            account.printDetails();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAccount ca = new CurrentAccount();
+            Account a = new Account(ca);
+            a.printAccounts();
+
+            IAccount sa = new SavingsAccount();
+            Account a1 = new Account(sa);
+            a1.printAccounts();
+        }
+    }
+}
---
+++ Property_Injection/Property_Injection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Property_Injection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
