f92884de70f1fd8ea1b50f09afd08b3208762539
Suryash-00 <suryash35@gmail.com>
2022-11-02 14:37:21 +0530

    http client tutorial

---
+++ HttpClientApi/HttpClientApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherService _weatherService;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherService weatherService)
+        {
+            _logger = logger;
+            _weatherService = weatherService;
+        }
+
+        [HttpGet]
+        public async Task<string> Get(string cityName)
+        {
+            return await _weatherService.Get(cityName);
+        }
+    }
+}
---
+++ HttpClientApi/HttpClientApi/WeatherService.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientApi
+{
+    public interface IWeatherService
+    {
+        Task<string> Get(string cityName);
+    }
+    public class WeatherService
+    {
+        private HttpClient _httpClient;
+
+        public WeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> Get(string cityName)
+        {
+            var apiKey = "<GET YOUR API KEY HERE https://www.weatherapi.com/>";
+
+            string APIURL = $"?key={apiKey}&q={cityName}";
+            var response = await _httpClient.GetAsync(APIURL);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
